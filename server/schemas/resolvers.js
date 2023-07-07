5c14f517afefb73598dbdcba3f1b9c70c4a35ae6
jbowdle <johnwbowdle@gmail.com>
2023-07-06 21:08:15 -0500

    added User reducers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,17 @@
-const resolvers = {
+const { User } = require('../models');
 
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find({});
+    },
+  },
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+  },
 }
 
 module.exports = resolvers;
