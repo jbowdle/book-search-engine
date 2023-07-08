66ab4080be5375cac1dd115c5d3ba949e2ffd5ac
jbowdle <johnwbowdle@gmail.com>
2023-07-07 22:26:05 -0500

    Created typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,38 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: Int
+    authors: [String!]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: Int
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(author: [String!], description: String, title: String!, bookId: Int, image: String, link: String): User
+    removeBook(bookId: Int): User
+  }
+`;
 
 module.exports = typeDefs;
